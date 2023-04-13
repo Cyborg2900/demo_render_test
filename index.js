8344b52028e62a385b40f9d401439dbf2dddaf25
Cyborg 9 <sarthaksns@gmail.com>
2023-04-13 16:13:32 +0530

    added files for testing

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express=require('express');
+
+const bodyParser = require('body-parser');
+const path=require('path');
+
+
+const app=express();
+
+const PORT=process.env.PORT || 5005;
+
+app.use(bodyParser.json());
+app.use(express.static('./front_end'));
+
+
+app.get('/testing',(req,res)=>{
+    console.log(req);
+    console.log(req.body);
+    console.log(req.params);
+    console.log(req.query);
+    res.status(200).send();
+})
+
+
+app.get('/',(req,res)=>{
+    console.log(process.env.PORT);
+    res.sendFile(path.resolve(__dirname,'./front_end/index.html'));
+})
+app.get('/second',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'./front_end/index.html'));
+})
+
+
+app.listen(PORT,()=>{
+    console.log('listenging to ',PORT);
+})
