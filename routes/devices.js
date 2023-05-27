6ffe7c3bc0bc3abcd881b86eea8a1cd9d816ee91
Cyborg 9 <sarthaksns@gmail.com>
2023-05-27 23:51:06 +0530

    small changes

--- routes/devices.js
+++ routes/devices.js
@@ -12,7 +12,7 @@ router.route('/get')
       //
       console.log(device, " post");
       D_model.findOne({uid:id}).then((data)=>{
-        if(data.type==1){
+        if(data.device_type==1){
             D_model.updateOne({uid:id},{$set: {last_time:Date.now() , status_temp:device.temp , status_hum:device.hum}}).then(()=>{
               if(data.status_1==device.status_1 && data.status_2==device.status_2 && data.status_3==device.status_3 && data.status_4==device.status_4){
                 D_model.updateOne({uid:id},{$set:{sync:true}}).then(()=>{
@@ -38,7 +38,7 @@ router.route('/get')
               console.log(err); 
               res.status(200).send("error occured");
             })
-        }else if(data.type==2){
+        }else if(data.device_type==2){
           D_model.updateOne({uid:id},{$set: {last_time:Date.now(), status_motion:device.motion}}).then(()=>{
             if(data.status_1==device.status_1 && data.status_2==device.status_2 && data.status_3==device.status_3 && data.status_4==device.status_4){
               D_model.updateOne({uid:id},{$set:{sync:true}}).then(()=>{
@@ -83,7 +83,7 @@ router.route('/get')
           res.send('no such device with this id is in db');
           return;
         }
-        if(device.type==1){
+        if(device.device_type==1){
           D_model.updateOne({uid:id},{$set:{status_1:status_1,status_2:status_2,status_3:status_3,status_4:status_4, status_temp:temp , status_hum:hum ,last_time:Date.now() }}).then(()=>{
             res.send('changes made to db');
           }).catch((err)=>{
