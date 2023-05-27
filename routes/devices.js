5a2980f39f6a5055c1cd6f6a9cc542de01bec2f0
Cyborg 9 <sarthaksns@gmail.com>
2023-05-27 23:46:00 +0530

    small changes

--- routes/devices.js
+++ routes/devices.js
@@ -9,20 +9,21 @@ router.route('/get')
 
       const id=req.body.id;
       const device=req.body;
+      //
       console.log(device, " post");
       D_model.findOne({uid:id}).then((data)=>{
         if(data.type==1){
             D_model.updateOne({uid:id},{$set: {last_time:Date.now() , status_temp:device.temp , status_hum:device.hum}}).then(()=>{
               if(data.status_1==device.status_1 && data.status_2==device.status_2 && data.status_3==device.status_3 && data.status_4==device.status_4){
                 D_model.updateOne({uid:id},{$set:{sync:true}}).then(()=>{
-                    res.send('device are in sync');
+                    res.status(200).send('device are in sync');
                 }).catch((err)=>{    
                   console.log(err); 
-                  res.send("error occured");
+                  res.status(200).send("error occured");
                 })
               }else{
                 D_model.updateOne({uid:id},{$set:{sync:false}}).then(()=>{
-                  res.send({
+                  res.status(200).send({
                     status_1:data.status_1,
                     status_2:data.status_2,
                     status_3:data.status_3,
@@ -30,25 +31,25 @@ router.route('/get')
                   })
                 }).catch((err)=>{
                   console.log(err); 
-                  res.send("error occured");
+                  res.status(200).send("error occured");
                 })
               }
             }).catch((err)=>{
               console.log(err); 
-              res.send("error occured");
+              res.status(200).send("error occured");
             })
         }else if(data.type==2){
           D_model.updateOne({uid:id},{$set: {last_time:Date.now(), status_motion:device.motion}}).then(()=>{
             if(data.status_1==device.status_1 && data.status_2==device.status_2 && data.status_3==device.status_3 && data.status_4==device.status_4){
               D_model.updateOne({uid:id},{$set:{sync:true}}).then(()=>{
-                  res.send('device are in sync');
+                  res.status(200).send('device are in sync');
               }).catch((err)=>{
                 console.log(err); 
                 res.send("error occured");
               })
             }else{
               D_model.updateOne({uid:id},{$set:{sync:false}}).then(()=>{
-                res.send({
+                res.status(200).send({
                   status_1:data.status_1,
                   status_2:data.status_2,
                   status_3:data.status_3,
@@ -56,17 +57,17 @@ router.route('/get')
                 })
               }).catch((err)=>{
                 console.log(err); 
-                res.send("error occured");
+                res.status(200).send("error occured");
               })
             }
           }).catch((err)=>{
             console.log(err); 
-            res.send("error occured");
+            res.status(200).send("error occured");
           })
         }
       }).catch((err)=>{
         console.log(err); 
-        res.send("error occured");
+        res.status(200).send("error occured");
       })
       
     
@@ -74,20 +75,23 @@ router.route('/get')
     .put(async(req,res)=>{
       console.log(req.body,'\n\n');
       const {id,status_1,status_2,status_3,status_4}=req.body;
+      const temp=req.body.temp;
+      const hum=req.body.hum;
+      const motion=req.body.motion;
       D_model.findOne({uid:id}).then((device)=>{
         if(device==null){
           res.send('no such device with this id is in db');
           return;
         }
         if(device.type==1){
-          D_model.updateOne({uid:id},{$set:{status_1:status_1,status_2:status_2,status_3:status_3,status_4:status_4, status_temp:req.body.temp , status_hum:req.body.hum ,last_time:Date.now() }}).then(()=>{
+          D_model.updateOne({uid:id},{$set:{status_1:status_1,status_2:status_2,status_3:status_3,status_4:status_4, status_temp:temp , status_hum:hum ,last_time:Date.now() }}).then(()=>{
             res.send('changes made to db');
           }).catch((err)=>{
             console.log(err); 
             res.send("error occured");
           })
         }else {
-          D_model.updateOne({uid:id},{$set:{status_1:status_1,status_2:status_2,status_3:status_3,status_4:status_4, status_motion:req.body.motion ,last_time:Date.now() }}).then(()=>{
+          D_model.updateOne({uid:id},{$set:{status_1:status_1,status_2:status_2,status_3:status_3,status_4:status_4, status_motion:motion ,last_time:Date.now() }}).then(()=>{
             res.send('changes made to db');
           }).catch((err)=>{
             console.log(err); 
